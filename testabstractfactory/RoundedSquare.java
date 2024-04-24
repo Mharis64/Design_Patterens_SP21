0187e629749b8e18c71853fd87aec0e300fb6b73
Muhammad Haris <114659151+Mharis64@users.noreply.github.com>
2024-04-24 14:00:39 +0500

    Lab Task Abstract Factory Design pattern added
    clone the file and open it in netbeans for testing if the code is working or not

---
+++ testabstractfactory/RoundedSquare.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testabstractfactory;
+
+/**
+ *
+ * @author sp21-bse-019
+ */
+public class RoundedSquare implements Shape {
+    
+    public void draw(){
+        System.out.println("Rounded Shape implemented");
+    }
+    
+}
---
+++ testabstractfactory/Square.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testabstractfactory;
+
+/**
+ *
+ * @author sp21-bse-019
+ */
+public class Square implements Shape {
+
+    public Square() {
+    }
+
+    @Override
+    public void draw() {
+      
+        System.out.println("Square implemented");
+
+    }
+    
+}
